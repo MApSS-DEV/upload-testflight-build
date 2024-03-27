e8edf59430b579e5c6318c2d46422c81d0a82989
jusalston <100620931+jusalston@users.noreply.github.com>
2024-03-27 10:14:46 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -15014,15 +15014,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(__webpack_require__(470));
 const command_1 = __webpack_require__(431);
 const os = __webpack_require__(87);
 const path = __webpack_require__(622);
@@ -15093,7 +15085,7 @@ exports.getInput = getInput;
  */
 function setOutput(name, value) {
     //command_1.issueCommand('set-output', { name }, value);
-    core.setOutput(name, value);
+    process.stdout.write(`"{${name}}={${value}}" >> $GITHUB_OUTPUT` + os.EOL);
 }
 exports.setOutput = setOutput;
 //-----------------------------------------------------------------------
