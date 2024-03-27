bf3a8e2824da6906ca9c351074554d15d2a9b79a
jusalston <100620931+jusalston@users.noreply.github.com>
2024-03-27 09:50:27 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -15014,6 +15014,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(470));
 const command_1 = __webpack_require__(431);
